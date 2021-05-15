5b856d8615b3d8e2551ac69211b7c40ad05f317c
Ceyhun Gulbas <44509662+ceyhungulbas@users.noreply.github.com>
2021-05-16 00:36:46 +0300

    Fixed Input Area
    no longer text

--- randotron/script.js
+++ randotron/script.js
@@ -1,6 +1,19 @@
 const submitButton = document.getElementById("thisIsSubmit")
 let result = document.getElementById("result")
 
+
+function isInputNumber(evt){
+                
+  let ch = String.fromCharCode(evt.which);
+              
+  if(!(/[0-9]/.test(ch))){
+  evt.preventDefault();
+  }
+              
+}
+
+
+
 function randotron(){
   let valueInput = document.getElementById("inputNumber").value   // Taking Value
   let randomHehe = Math.floor(Math.random() * valueInput);        //Random generator
@@ -10,3 +23,22 @@ function randotron(){
 }
 
 submitButton.addEventListener("click", randotron)
+
+
+{/* <div class="challengeTwo">
+<input type="text" id="myText" onkeypress="isInputNumber(event)">
+<button onclick="increaseCounter()">Increase Counter</button>
+<button onclick="decreaseCounter()">Decrease Counter</button>
+<p>Count: <span id="resCountbutID">5</span></p>
+</div>
+
+
+function isInputNumber(evt){
+                
+  let ch = String.fromCharCode(evt.which);
+              
+  if(!(/[0-9]/.test(ch))){
+  evt.preventDefault();
+  }
+                
+} */}
